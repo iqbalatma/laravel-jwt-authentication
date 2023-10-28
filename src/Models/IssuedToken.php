fb5ca69b1f09306137786cec0e0ee7fd602989aa
iqbalatma <iqbalatma@gmail.com>
2023-10-28 19:24:20 +0700

    create method on IssuedTokens for query

--- src/Models/IssuedToken.php
+++ src/Models/IssuedToken.php
@@ -2,18 +2,18 @@
 
 namespace Iqbalatma\LaravelJwtAuthentication\Models;
 
+use App\Enums\TokenType;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Iqbalatma\LaravelJwtAuthentication\Enums\TokenType;
 
 
 /**
  * @property string id
  * @property string subject_id
  * @property string jti
- * @property string token_type
+ * @property TokenType token_type
  * @property string user_agent
  * @property Carbon expired_at
  * @property Carbon created_at
@@ -22,7 +22,30 @@ use Iqbalatma\LaravelJwtAuthentication\Enums\TokenType;
 class IssuedToken extends Model
 {
     use HasFactory, HasUuids;
+
     protected $fillable = [
         "subject_id", "jti", "token_type", "user_agent", "expired_at"
     ];
+
+
+    /**
+     * @param string $jti
+     * @return IssuedToken|null
+     */
+    public static function getTokenByJTI(string $jti): IssuedToken|null
+    {
+        return (new static())->where("jti", $jti)->first();
+    }
+
+    /**
+     * @param string $subjectId
+     * @return IssuedToken|null
+     */
+    public static function getTokenBySubjectId(string $subjectId): IssuedToken|null
+    {
+        return (new static())->where("subject_id", $subjectId)->first();
+    }
+
+
+
 }
