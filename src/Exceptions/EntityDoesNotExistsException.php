4c53e34aa82f7907756059b3ee963104912f8c83
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2023-10-29 01:44:18 +0700

    token revocation service done

---
+++ src/Exceptions/EntityDoesNotExistsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iqbalatma\LaravelJwtAuthentication\Exceptions;
+
+use Exception;
+use Throwable;
+
+class EntityDoesNotExistsException extends Exception
+{
+    public function __construct(string $message = "Entity does not exists", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
--- src/IssuedTokenService.php
+++ src/IssuedTokenService.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Iqbalatma\LaravelJwtAuthentication\Enums\TokenType;
+use Iqbalatma\LaravelJwtAuthentication\Exceptions\EntityDoesNotExistsException;
 use Iqbalatma\LaravelJwtAuthentication\Traits\BlacklistTokenHelper;
 
 class IssuedTokenService
@@ -64,5 +65,71 @@ class IssuedTokenService
         return $tokens->filter(function ($item) {
             return $item["type"] === TokenType::ACCESS->value && $item["iat"] < app(JWTService::class)->getRequestedTokenPayloads("iat");
         })->values();
+    }
+
+
+    /**
+     * @param string $userAgent
+     * @param string|int|null $subjectId
+     * @return void
+     * @throws EntityDoesNotExistsException
+     */
+    public static function revokeTokenRefreshByUserAgent(string $userAgent, string|int|null $subjectId = null): void
+    {
+        /**
+         * todo: this line of code can move into single method
+         */
+        if (!$subjectId) {
+            $subjectId = Auth::id();
+        }
+
+        $instance = (new static());
+        $instance->setSubjectCacheRecord($subjectId);
+
+        if (!$instance->isTokenBlacklistByTypeAndUserAgentExists(TokenType::REFRESH->value, $userAgent)){
+            throw new EntityDoesNotExistsException("Token on device $userAgent does not exists");
+        }
+
+        $instance->updateExistingBlacklistTokenByTypeAndUserAgent(TokenType::REFRESH->value, $userAgent);
+        $instance->updateSubjectCacheRecord($subjectId);
+    }
+
+    /**
+     * @param string $userAgent
+     * @param string|int|null $subjectId
+     * @return void
+     * @throws EntityDoesNotExistsException
+     */
+    public static function revokeTokenAccessByUserAgent(string $userAgent, string|int|null $subjectId = null): void
+    {
+        /**
+         * todo: this line of code can move into single method
+         */
+        if (!$subjectId) {
+            $subjectId = Auth::id();
+        }
+
+        $instance = (new static());
+        $instance->setSubjectCacheRecord($subjectId);
+
+        if (!$instance->isTokenBlacklistByTypeAndUserAgentExists(TokenType::ACCESS->value, $userAgent)){
+            throw new EntityDoesNotExistsException("Token on device $userAgent does not exists");
+        }
+
+        $instance->updateExistingBlacklistTokenByTypeAndUserAgent(TokenType::ACCESS->value, $userAgent);
+        $instance->updateSubjectCacheRecord($subjectId);
+    }
+
+
+    /**
+     * @throws EntityDoesNotExistsException
+     */
+    public static function revokeTokenByUserAgent(string $userAgent, string|int|null $subjectId = null): void
+    {
+        if (!$subjectId) {
+            $subjectId = Auth::id();
+        }
+        self::revokeTokenAccessByUserAgent($userAgent, $subjectId);
+        self::revokeTokenRefreshByUserAgent($userAgent, $subjectId);
     }
 }
